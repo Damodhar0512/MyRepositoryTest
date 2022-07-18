6e9951dfe0ccb24a86db61743b0d8b78aa780ae0
DAMODHAR <ndamoo@gmail.com>
2022-07-18 20:55:14 +0530

    This is my First Code Push in git hub In My Way Second time

--- mongotest1.py
+++ mongotest1.py
@@ -14,3 +14,13 @@ d = {
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
+
+d = {
+    "name" : "Damodhar",
+    "email" : "Damodhar@gmail.com" ,
+    "surname" : "sree"
+ }
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
